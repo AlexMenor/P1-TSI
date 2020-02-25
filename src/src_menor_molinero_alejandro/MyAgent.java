7be338025d00776ef7f401a3cdaad495d72c19c5
Alex Menor <alex4menor@gmail.com>
2020-02-25 13:08:52 +0100

    added maps and made some representation of the world

--- src/src_menor_molinero_alejandro/MyAgent.java
+++ src/src_menor_molinero_alejandro/MyAgent.java
@@ -1,30 +1,58 @@
 package src_menor_molinero_alejandro;
 
+import core.game.Observation;
 import core.game.StateObservation;
 import core.player.AbstractPlayer;
 import ontology.Types;
 import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
+import tools.Vector2d;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class MyAgent extends AbstractPlayer{
 
+    private char [][] myGrid;
+    private Vector2d doorPosition;
 
-    /**
-     * initialize all variables for the agent
-     * @param stateObs Observation of the current state.
-     * @param elapsedTimer Timer when the action returned is due.
-     */
+
     public MyAgent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+        generateStaticMap(stateObs);
+        for(int i = 0 ; i < myGrid.length ; i++) {
+            for (int j = 0; j < myGrid[i].length; j++)
+                System.out.print(myGrid[i][j] + " ");
+            System.out.println();
+        }
     }
 
-    /**
-     * return ACTION_NIL on every call to simulate doNothing player
-     * @param stateObs Observation of the current state.
-     * @param elapsedTimer Timer when the action returned is due.
-     * @return 	ACTION_NIL all the time
-     */
+    private void generateStaticMap (StateObservation stateOBs){
+        Dimension worldDimension = stateOBs.getWorldDimension();
+        int xLength = worldDimension.width / 30;
+        int yLength = worldDimension.height / 30;
+
+        myGrid = new char [xLength][yLength];
+
+        ArrayList<Observation> walls = stateOBs.getImmovablePositions()[0];
+
+        for (Observation wall : walls){
+            int wallX = ((int)wall.position.x) / 30;
+            int wallY = ((int)wall.position.y) / 30;
+
+            myGrid[wallX][wallY] = 'w';
+        }
+
+        System.out.println(stateOBs.getPortalsPositions()[0]);
+    }
+
+    private void getDoorPosition(StateObservation stateObs){
+       Observation observation = stateObs.getPortalsPositions()[0].get(0);
+       this.doorPosition = observation.position;
+    }
+
     @Override
     public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+
         return Types.ACTIONS.ACTION_NIL;
     }
 }
